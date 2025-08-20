305849b798eeba0c4f00d6fb89944c546d64540b
Hradecky Miloslav <miloslavhradecky76@gmail.com>
2025-08-20 11:56:55 +0200

    🧱 New class PrintLogicController.

--- controllers/print_logic_controller.py
+++ controllers/print_logic_controller.py
@@ -12,6 +12,13 @@ class PrintLogicController:
         self.finalize = finalize_callback  # ✅ method from the main controller
 
     def product_save_and_print(self, header: str, record: str, trigger_values: list[str]) -> None:
+        """
+        Extracts header and record for the scanned serial number and writes them to product output file.
+
+            :param header: extracted header line
+            :param record: extracted record line
+            :param trigger_values: list of trigger filenames
+        """
         raw_output_path = self.config.get("ProductPaths", "output_file_path_product")
         if not raw_output_path:
             self.logger.warning("Cesta k výstupnímu souboru product nebyla nalezena.")
@@ -47,6 +54,16 @@ class PrintLogicController:
             self.print_window.reset_input_focus()
 
     def control4_save_and_print(self, header: str, record: str, trigger_values: list[str]) -> None:
+        """
+        Extracts header and record for the scanned serial number and writes them to Control4 output file.
+
+            - Searches for lines beginning with: SERIAL+I= and SERIAL+J= and SERIAL+K=
+            - If it finds both the header and the record, it writes them to the output file.
+
+            :param header: extracted header line
+            :param record: extracted record line
+            :param trigger_values: list of trigger filenames
+        """
         raw_output_path = self.config.get("Control4Paths", "output_file_path_c4_product")
         if not raw_output_path:
             self.logger.error("Cesta k výstupnímu souboru Control4 nebyla nalezena.")
@@ -82,6 +99,13 @@ class PrintLogicController:
             self.print_window.reset_input_focus()
 
     def my2n_save_and_print(self, serial_number: str, token: str) -> None:
+        """
+        Writes extracted My2N token and serial number to output file and creates trigger file.
+
+            :param serial_number: serial number from input
+            :param token: extracted My2N token
+            :param output_path: path to output file
+        """
         raw_output_path = self.config.get("My2nPaths", "output_file_path_my2n")
         if not raw_output_path:
             self.logger.error("Cesta k výstupnímu souboru My2N nebyla nalezena.")
@@ -116,6 +140,9 @@ class PrintLogicController:
             self.print_window.reset_input_focus()
 
     def _get_trigger_dir(self) -> Path | None:
+        """
+        Returns trigger directory path from config.
+        """
         raw_path = self.config.get("Paths", "trigger_path")
         if not raw_path:
             self.logger.error("Trigger path není definován.")
